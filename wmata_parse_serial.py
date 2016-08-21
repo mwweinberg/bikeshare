93b9e03081893565b8cf9b6c5795087e9cb0570a
mwweinberg <mwweinberg@users.noreply.github.com>
2016-08-21 18:13:54 -0400

    added While True & serial commands
    need to uncomment the serial stuff once an arduino is connected

--- wmata_parse_serial.py
+++ wmata_parse_serial.py
@@ -1,192 +1,209 @@
 import urllib, json
+import time
+import serial
 url = "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/E03?api_key=547990fbdfe344f49dfc4df55a88c26a"
 
 green_list = []
 yellow_list = []
 wmata_serial = []
 
+#TODO: uncomment the arduino ser parts here and at the very end
+"""
+#opens up the serial connection with arduino
+ser = serial.Serial('/dev/ttyACM0', 9600)
+#this is necessary because once it opens up the serial port arduino needs a second
+time.sleep(2)
+"""
 
 
 #this is the syntax to get the line for the first train from the data
 #print w[0]['Line']
 
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-w = data['Trains']
+while True:
+
+	#clears out data from last loop
+	wmata_serial = []
 
 
-# this iterates through all of the trains and adds all the times for southbound trains either the list green_list[] or yellow_list[].
-
-for i in range (0,4):
-
-	if w[i]['Line'] == 'GR':
-
-		#this is a northern-bound train so it doesn't matter for the final product
-
-		if w[i]['Destination'] == 'Grnbelt':
-			print "A green train that is going to Greenbelt will arrive in %s minutes" % (w[i]['Min'])
-
-		#this is the southern train so they get added to the list
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	w = data['Trains']
 
 
-		elif w[i]['Destination'] == 'Brnch Av':
-			print "A green train that is going to Branch Ave. will arrive in %s minutes" % (w[i]['Min'])
+	# this iterates through all of the trains and adds all the times for southbound trains either the list green_list[] or yellow_list[].
 
-			green_list.append(w[i]['Min'])
+	for i in range (0,4):
 
+		if w[i]['Line'] == 'GR':
 
+			#this is a northern-bound train so it doesn't matter for the final product
 
+			if w[i]['Destination'] == 'Grnbelt':
+				print "A green train that is going to Greenbelt will arrive in %s minutes" % (w[i]['Min'])
 
-		else:
-			print "Another metro data screwup"
-
-
-
-	elif w[i]['Line'] == 'YL':
-
-		#this is a northern-bound train so it doesn't matter for the final product
-
-		if w[i]['Destination'] == 'Grnbelt':
-			print "A yellow train that is going to Greenbelt will arrive in %s minutes" % (w[i]['Min'])
-
-		elif w[i]['Destination'] == 'Ft.Tottn':
-			print "A yellow train that is going to Ft. Totten will arrive in %s minutes" % (w[i]['Min'])
-
-		#these are the two desinations of the southern trains so they get added to the list
-
-		elif w[i]['Destination'] == 'Hntingtn':
-			print "A yellow train that is going to Huntington and will arrive in %s minutes" % (w[i]['Min'])
-
-			yellow_list.append(w[i]['Min'])
-
-		elif w[i]['Destination'] == 'Brnch Av':
-			print "A yellow train that is going to Franconia-Springfield will arrive in %s minutes" % (w[i]['Min'])
-
-			yellow_list.append(w[i]['Min'])
+			#this is the southern train so they get added to the list
 
 
-		else:
-			print "Another metro data screwup"
+			elif w[i]['Destination'] == 'Brnch Av':
+				print "A green train that is going to Branch Ave. will arrive in %s minutes" % (w[i]['Min'])
 
-# these lists now have the arrival times for as many as the next four trains
-
-print green_list
-print yellow_list
-
-# TODO: now assign lights based on the first two numbers in the green and yellow lists
+				green_list.append(w[i]['Min'])
 
 
 
-#this chunk assigns the light position for the first green train
-#this limit is to avoid a range error if the isn't a value in the first position
-if len(green_list) >= 1:
 
-	green_one = green_list[0]
-
-	if green_one == 'ARR':
-			print "you missed the next southbound green train because it is arriving"
-			wmata_serial.append('a')
-
-	elif green_one == 'BRD':
-			print "you missed the next southbound green train because it is boarding"
-			wmata_serial.append('a')
-
-	elif int(green_one) <= 4:
-			print "you missed the next southbound green train"
-			wmata_serial.append('a')
-	elif 5 <= int(green_one) <= 9:
-			print "leave now for the next southbound green train!"
-			wmata_serial.append('b')
-	elif 10 <= int(green_one) <= 15:
-			print "the next southbound green train is coming soon"
-			wmata_serial.append('c')
-	elif 16 <= int(green_one) <= 25:
-			print "the next southbound green train is a long way off"
-			wmata_serial.append('d')
-
-#this chunk assigns the light position for the second green train
-
-if len(green_list) >= 2:
-
-	green_two = green_list[1]
-
-	if green_two == 'ARR':
-			print "you missed the next southbound green train because it is arriving"
-			wmata_serial.append('e')
-
-	elif green_two == 'BRD':
-			print "you missed the next southbound green train because it is boarding"
-			wmata_serial.append('e')
-
-	if int(green_two) <= 4:
-			print "you missed the next southbound green train"
-			wmata_serial.append('e')
-	elif 5 <= int(green_two) <= 9:
-			print "leave now for the next southbound green train!"
-			wmata_serial.append('f')
-	elif 10 <= int(green_two) <= 15:
-			print "the next southbound green train is coming soon"
-			wmata_serial.append('g')
-	elif 16 <= int(green_two) <= 25:
-			print "the next southbound green train is a long way off"
-			wmata_serial.append('h')
+			else:
+				print "Another metro data screwup"
 
 
-if len(yellow_list) >= 1:
 
-	yellow_one = yellow_list[0]
+		elif w[i]['Line'] == 'YL':
 
-	if yellow_one == 'ARR':
-			print "you missed it because it is arriving"
-			wmata_serial.append('i')
+			#this is a northern-bound train so it doesn't matter for the final product
 
-	elif yellow_one == 'BRD':
-			print "you missed it because it is boarding"
-			wmata_serial.append('i')
+			if w[i]['Destination'] == 'Grnbelt':
+				print "A yellow train that is going to Greenbelt will arrive in %s minutes" % (w[i]['Min'])
 
-	elif int(yellow_one) <= 4:
-			print "you missed it"
-			wmata_serial.append('i')
-	elif 5 <= int(yellow_one) <= 9:
-			print "leave now!"
-			wmata_serial.append('j')
-	elif 10 <= int(yellow_one) <= 15:
-			print "coming soon"
-			wmata_serial.append('k')
-	elif 16 <= int(yellow_one) <= 25:
-			print "long way off"
-			wmata_serial.append('l')
+			elif w[i]['Destination'] == 'Ft.Tottn':
+				print "A yellow train that is going to Ft. Totten will arrive in %s minutes" % (w[i]['Min'])
 
-#this chunk assigns the light position for the second yellow train
+			#these are the two desinations of the southern trains so they get added to the list
 
-if len(yellow_list) >= 2:
+			elif w[i]['Destination'] == 'Hntingtn':
+				print "A yellow train that is going to Huntington and will arrive in %s minutes" % (w[i]['Min'])
 
-	yellow_two = yellow_list[1]
+				yellow_list.append(w[i]['Min'])
 
-	if yellow_two == 'ARR':
-			print "you missed it because it is arriving"
-			wmata_serial.append('m')
+			elif w[i]['Destination'] == 'Brnch Av':
+				print "A yellow train that is going to Franconia-Springfield will arrive in %s minutes" % (w[i]['Min'])
 
-	elif yellow_two == 'BRD':
-			print "you missed it because it is boarding"
-			wmata_serial.append('m')
+				yellow_list.append(w[i]['Min'])
 
-	if int(yellow_two) <= 4:
-			print "you missed it"
-			wmata_serial.append('m')
-	elif 5 <= int(yellow_two) <= 9:
-			print "leave now!"
-			wmata_serial.append('n')
-	elif 10 <= int(yellow_two) <= 15:
-			print "coming soon"
-			wmata_serial.append('o')
-	elif 16 <= int(yellow_two) <= 25:
-			print "long way off"
-			wmata_serial.append('p')
 
-#adds the terminating character
-wmata_serial.append('z')
-#turns wmata_serial into a string
-wmata_serial_str = ''.join(wmata_serial)
-print wmata_serial
-print wmata_serial_str
+			else:
+				print "Another metro data screwup"
+
+	# these lists now have the arrival times for as many as the next four trains
+
+	print green_list
+	print yellow_list
+
+
+
+
+
+	#this chunk assigns the light position for the first green train
+	#this limit is to avoid a range error if the isn't a value in the first position
+	if len(green_list) >= 1:
+
+		green_one = green_list[0]
+
+		if green_one == 'ARR':
+				print "you missed the next southbound green train because it is arriving"
+				wmata_serial.append('a')
+
+		elif green_one == 'BRD':
+				print "you missed the next southbound green train because it is boarding"
+				wmata_serial.append('a')
+
+		elif int(green_one) <= 4:
+				print "you missed the next southbound green train"
+				wmata_serial.append('a')
+		elif 5 <= int(green_one) <= 9:
+				print "leave now for the next southbound green train!"
+				wmata_serial.append('b')
+		elif 10 <= int(green_one) <= 15:
+				print "the next southbound green train is coming soon"
+				wmata_serial.append('c')
+		elif 16 <= int(green_one) <= 25:
+				print "the next southbound green train is a long way off"
+				wmata_serial.append('d')
+
+	#this chunk assigns the light position for the second green train
+
+	if len(green_list) >= 2:
+
+		green_two = green_list[1]
+
+		if green_two == 'ARR':
+				print "you missed the next southbound green train because it is arriving"
+				wmata_serial.append('e')
+
+		elif green_two == 'BRD':
+				print "you missed the next southbound green train because it is boarding"
+				wmata_serial.append('e')
+
+		if int(green_two) <= 4:
+				print "you missed the next southbound green train"
+				wmata_serial.append('e')
+		elif 5 <= int(green_two) <= 9:
+				print "leave now for the next southbound green train!"
+				wmata_serial.append('f')
+		elif 10 <= int(green_two) <= 15:
+				print "the next southbound green train is coming soon"
+				wmata_serial.append('g')
+		elif 16 <= int(green_two) <= 25:
+				print "the next southbound green train is a long way off"
+				wmata_serial.append('h')
+
+
+	if len(yellow_list) >= 1:
+
+		yellow_one = yellow_list[0]
+
+		if yellow_one == 'ARR':
+				print "you missed it because it is arriving"
+				wmata_serial.append('i')
+
+		elif yellow_one == 'BRD':
+				print "you missed it because it is boarding"
+				wmata_serial.append('i')
+
+		elif int(yellow_one) <= 4:
+				print "you missed it"
+				wmata_serial.append('i')
+		elif 5 <= int(yellow_one) <= 9:
+				print "leave now!"
+				wmata_serial.append('j')
+		elif 10 <= int(yellow_one) <= 15:
+				print "coming soon"
+				wmata_serial.append('k')
+		elif 16 <= int(yellow_one) <= 25:
+				print "long way off"
+				wmata_serial.append('l')
+
+	#this chunk assigns the light position for the second yellow train
+
+	if len(yellow_list) >= 2:
+
+		yellow_two = yellow_list[1]
+
+		if yellow_two == 'ARR':
+				print "you missed it because it is arriving"
+				wmata_serial.append('m')
+
+		elif yellow_two == 'BRD':
+				print "you missed it because it is boarding"
+				wmata_serial.append('m')
+
+		if int(yellow_two) <= 4:
+				print "you missed it"
+				wmata_serial.append('m')
+		elif 5 <= int(yellow_two) <= 9:
+				print "leave now!"
+				wmata_serial.append('n')
+		elif 10 <= int(yellow_two) <= 15:
+				print "coming soon"
+				wmata_serial.append('o')
+		elif 16 <= int(yellow_two) <= 25:
+				print "long way off"
+				wmata_serial.append('p')
+
+	#adds the terminating character
+	wmata_serial.append('z')
+	#turns wmata_serial into a string
+	wmata_serial_str = ''.join(wmata_serial)
+	print wmata_serial
+	print wmata_serial_str
+	#ser.write(wmata_serial)
+	time.sleep(10)
