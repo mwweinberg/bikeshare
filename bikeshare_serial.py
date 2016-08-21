68b67aef854a909b15f9ff029eead8e6cc060bd7
mwweinberg <mwweinberg@users.noreply.github.com>
2016-08-21 09:07:31 -0400

    added serial and serial responder

---
+++ bikeshare_serial.py
@@ -0,0 +1,95 @@
+# from http://www.plotsofdots.com/archives/68
+
+
+import xml.etree.ElementTree as ET
+import urllib2
+# this is for the pause
+import time
+import serial
+
+# parse the data using urlib2 and xml
+site='https://www.capitalbikeshare.com/data/stations/bikeStations.xml'
+htm=urllib2.urlopen(site)
+doc = ET.parse(htm)
+
+# define the stations that will be identified
+# note that these use the number in <id> not the <terminalName>
+# if you want to use <terminalName> change 'id' in the 'for country...' loop
+# to 'terminalName'
+
+station1 = '274' # 10th & Fl
+station2 = '43' # 10th & U
+station3 = '242' # 12th & U
+station4 = '15' # 14th & V
+
+
+ser_holder = []
+
+#opens up the serial connection with arduino
+ser = serial.Serial('/dev/ttyACM0', 9600)
+#this is necessary because once it opens up the serial port arduino needs a second
+time.sleep(2)
+
+
+
+#we get the root tag
+root=doc.getroot()
+root.tag
+
+#we define empty lists for the empty bikes
+sID=[]
+embikes=[]
+#we now use a for loop to extract the information we are interested in
+for country in root.findall('station'):
+	# takes the ID as a string
+	sID.append(country.find('id').text)
+	# takes the number of bikes as an integer
+	embikes.append(int(country.find('nbBikes').text))
+
+
+#use zip to create touples and then parse them into a dictionary
+prov=dict(zip(sID,embikes))
+
+#this print command was for testing and can be removed
+#print prov['15']
+
+# this function returns a color based on the number of bikes remaining
+# each serial write needs to be specific to a station
+def lighter(id, green, yellow, orange, red):
+	#get the number of bikes remaining based on station id
+	bk = prov[id]
+	print bk
+	#numbers below are GRB to sync with neopixels
+	if bk >= 6:
+		print "Station %s is green - 255, 0, 0" % id
+		ser_holder.append(green)
+	elif bk > 2:
+		print "Station %s is yellow - 255, 255, 0" % id
+		ser_holder.append(yellow)
+	elif bk > 0:
+		print "Station %s is orange - 128, 255, 0" % id
+		ser_holder.append(orange)
+	else:
+		print "Station %s is red - 0, 255, 0" % id
+		ser_holder.append(red)
+
+# now run the function taking all of the stations as an argument
+while True:
+	#this cleans out the last loop's ser_holder variables
+	ser_holder = []
+
+	lighter(station1, 'A', 'B', 'C', 'D')
+	lighter(station2, 'E', 'F', 'G', 'H')
+	lighter(station3, 'I', 'J', 'K', 'L')
+	lighter(station4, 'M', 'N', 'O', 'P')
+
+	#add terminating char to ser_holder for arudino reading
+	ser_holder.append('Z')
+
+	#turns the list ser_holder into string ser_holder_str
+	ser_holder_str = ''.join(ser_holder)
+	#TODO: add %/n or whatever terminal character is needed
+	print ser_holder
+	print ser_holder_str
+	ser.write(ser_holder)
+	time.sleep(10)
---
+++ bikeshare_serial_responder/bikeshare_serial_responder.ino
@@ -0,0 +1,127 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+int incomingByte; //variable to read incoming serial data
+String str;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+ 
+
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+   // initialize serial communication:
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // see if there's incoming serial data:
+  // TODO: read the string instead of just the most recent char
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    // TODO: make this variable the entire string
+    str = Serial.readStringUntil('Z');
+
+    //if var_for_string.indexOf("String")? > 0):
+      //make LED this color
+
+    //first station
+
+    if (str.indexOf('A') >= 0) {
+      strip.setPixelColor(0, 255, 0, 0);
+    }
+
+    if (str.indexOf('B') >= 0) {
+      strip.setPixelColor(0, 255, 255, 0);
+    }
+
+    if (str.indexOf('C') >= 0) {
+      strip.setPixelColor(0, 128, 255, 0);
+    }
+
+    if (str.indexOf('D') >= 0) {
+      strip.setPixelColor(0, 0, 255, 0);
+    }
+
+
+    //second station
+
+    if (str.indexOf('E') >= 0) {
+      strip.setPixelColor(1, 255, 0, 0);
+    }
+
+    if (str.indexOf('F') >= 0) {
+      strip.setPixelColor(1, 255, 255, 0);
+    }
+
+    if (str.indexOf('G') >= 0) {
+      strip.setPixelColor(1, 128, 255, 0);
+    }
+
+    if (str.indexOf('H') >= 0) {
+      strip.setPixelColor(1, 0, 255, 0);
+    }
+
+    //third station
+
+    if (str.indexOf('I') >= 0) {
+      strip.setPixelColor(2, 255, 0, 0);
+    }
+
+    if (str.indexOf('J') >= 0) {
+      strip.setPixelColor(2, 255, 255, 0);
+    }
+
+    if (str.indexOf('K') >= 0) {
+      strip.setPixelColor(2, 128, 255, 0);
+    }
+
+    if (str.indexOf('L') >= 0) {
+      strip.setPixelColor(2, 0, 255, 0);
+    }
+
+    //fourth station
+
+    if (str.indexOf('I') >= 0) {
+      strip.setPixelColor(3, 255, 0, 0);
+    }
+
+    if (str.indexOf('J') >= 0) {
+      strip.setPixelColor(3, 255, 255, 0);
+    }
+
+    if (str.indexOf('K') >= 0) {
+      strip.setPixelColor(3, 128, 255, 0);
+    }
+
+    if (str.indexOf('L') >= 0) {
+      strip.setPixelColor(3, 0, 255, 0);
+    }
+   
+  }
+
+ 
+
+  //pushes the changes to the LEDs
+  strip.show();
+ 
+}
